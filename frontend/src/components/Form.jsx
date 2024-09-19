91713243e2a264933e2509b7321c95c6121c73da
Buomkuoth Yian <buom.nyajal@gmail.com>
2024-09-19 12:52:15 +0300

    Done with frontend

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,6 +3,7 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css";
+import LoadingIndicator from "./LoadingIndicator";
 
 const Form = ({ route, method }) => {
   const [username, setUsername] = useState("");
@@ -58,6 +59,8 @@ const Form = ({ route, method }) => {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
+
+      {loading && <LoadingIndicator />}
 
       <button className="form-button" type="submit">
         {name}
